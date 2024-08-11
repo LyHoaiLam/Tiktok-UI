1460be85d4b3c9d27fe3e693149c5cfff28fc168
LyHoaiLam <hoailam03999@gmail.com>
2024-08-11 11:35:41 +0700

    push

---
+++ src/Components/Button/Button.js
@@ -0,0 +1,94 @@
+import { Link } from 'react-router-dom';
+import styles from './Button.module.scss';
+import classNames from 'classnames/bind';
+import PropTypes from 'prop-types';
+
+
+const cx = classNames.bind(styles);
+
+function Button({
+    to,
+    href,
+    primary = false,
+    outline = false,
+    small = false,
+    large = false,
+    text,
+    //Nếu để là text = true thì các button có backgroundColor cũng sẽ có viền dưới text khi Hover vào (Chúng ta chỉ muốn mỗi button có text khi Hover là có viền bên dưới thôi)
+    disable,
+    rounded = false,
+    leftIcon,
+    rightIcon,
+    children,
+    onClick,
+    className,
+    ...passProps
+}) {
+    let Comp = 'button';
+    const _props = {
+        onClick,
+        ...passProps,
+    };
+
+    if (disable) {
+        //Khi có class disbale thì không có Click (xóa event Click luôn)
+        // delete _props.onClick;
+
+        //Cách 2
+        Object.keys(_props).forEach((key) => {
+            if (key.startsWith('on') && typeof _props[key] === 'function') {
+                delete _props[key];
+            }
+        });
+    }
+
+    if (to) {
+        _props.to = to;
+        Comp = Link;
+    } else if (href) {
+        _props.href = href;
+        Comp = 'a';
+    }
+
+    const classes = cx('wrapper', {
+        primary,
+        outline,
+        small,
+        large,
+        text,
+        disable,
+        leftIcon,
+        rightIcon,
+        rounded,
+        //Sau này cần custome gì thì css cho nó vào class rồi add class đó vào bên cx bên kia
+        //Khi có className thì nó lấy giá trị className làm key ở đây (trong trường hợp này là custome-login bên Header)
+        [className]: className,
+    });
+
+    return (
+        <Comp className={classes} {..._props}>
+            {leftIcon && <span className={cx('icon')}> {leftIcon}</span>}
+            <span className={cx('title')}>{children}</span>
+            {rightIcon && <span className={cx('icon')}> {rightIcon}</span>}
+        </Comp>
+    );
+}
+
+Button.prototype = {
+    to: PropTypes.string,
+    href: PropTypes.string,
+    primary: PropTypes.bool,
+    outline: PropTypes.bool,
+    small: PropTypes.bool,
+    large: PropTypes.bool,
+    text: PropTypes.bool,
+    disable: PropTypes.bool,
+    rounded: PropTypes.bool,
+    children: PropTypes.node.isRequired,
+    leftIcon: PropTypes.node,
+    rightIcon: PropTypes.node,
+    onClick: PropTypes.string,
+    className: PropTypes.string,
+}
+
+export default Button;
---
+++ src/layouts/Popper/Menu/Menu.js
@@ -0,0 +1,79 @@
+import { useState } from 'react';
+import Tippy from '@tippyjs/react/headless';
+import PropTypes from 'prop-types';
+import classNames from 'classnames/bind';
+import { Wrapper as PopperWrapper } from '~/layouts/Popper/';
+import style from './Menu.module.scss';
+import MenuItem from './MenuItem';
+import Header from './Header';
+
+const cx = classNames.bind(style);
+
+const defaultFc = () => {};
+
+function Menu({ children, items = [], hideOnClick = false, onChange = defaultFc }) {
+    const [history, setHistory] = useState([{ data: items }]);
+    //Lấy phần tử cuối
+    const current = history[history.length - 1];
+
+    const renderItems = () => {
+        //Thg nào có childrent là object không có là undefined, thêm !! để convert thành boolean
+        return current.data.map((item, index) => {
+            const isParent = !!item.children;
+            return (
+                <MenuItem
+                    key={index}
+                    data={item}
+                    onClick={() => {
+                        if (isParent) {
+                            console.log(item.children);
+                            setHistory((prev) => [...prev, item.children]);
+                        } else {
+                            onChange(item);
+                        }
+                    }}
+                />
+            );
+        });
+    };
+
+    return (
+        <Tippy
+            // visible={true}
+            interactive
+            delay={[0, 1000]}
+            offset={[10, 10]} //Bên phải và chiều cao của Tippy
+            placement="bottom-end"
+            hideOnClick={hideOnClick}
+            render={(attrs) => (
+                <div className={cx('menu-list')} tabIndex="-1" {...attrs}>
+                    <PopperWrapper className={cx('menu-popper')}>
+                        {history.length > 1 && (
+                            <Header
+                                title={current.title}
+                                onBack={() => {
+                                    setHistory((prev) => prev.slice(0, prev.length - 1));
+                                }}
+                            />
+                        )}
+
+                        <div className={cx('menu-body')}>{renderItems()}</div>
+                    </PopperWrapper>
+                </div>
+            )}
+            onHide={() => setHistory((prev) => prev.slice(0, 1))}
+        >
+            {children}
+        </Tippy>
+    );
+}
+
+
+Menu.prototype = {
+    children: PropTypes.node.isRequired,
+    items: PropTypes.array,
+    hideOnClick: PropTypes.bool,
+    onChange: PropTypes.func
+}
+
+export default Menu;
